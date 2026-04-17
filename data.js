812152f25549ef1f1c14537b11ad5f7fa57f4cb8
davittaDE5 <manodavitta@gmail.com>
2026-04-17 14:27:51 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,308 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-powder-room",
+      "name": "Powder room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9138126599488581
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.487685711502028,
+          "pitch": 0.07842972403348547,
+          "rotation": 0.7853981633974483,
+          "target": "1-mothers-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-mothers-room",
+      "name": "Mothers Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9138126599488581
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7346704799188544,
+          "pitch": 0.00896248103377495,
+          "rotation": 0,
+          "target": "2-daughters-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-daughters-room",
+      "name": "Daughters room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9138126599488581
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.875170261469483,
+          "pitch": -0.02116661208069104,
+          "rotation": 5.497787143782138,
+          "target": "3-sf-bath"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-bath",
+      "name": "SF Bath",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9138126599488581
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.796529061063251,
+          "pitch": -0.066941993405921,
+          "rotation": 1.5707963267948966,
+          "target": "4-sf-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-toilet",
+      "name": "SF Toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9138126599488581
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.632889313945027,
+          "pitch": -0.10473060885305685,
+          "rotation": 4.71238898038469,
+          "target": "5-wudhu"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-wudhu",
+      "name": "Wudhu",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9138126599488581
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0169613618223856,
+          "pitch": -0.08608663355059498,
+          "rotation": 0,
+          "target": "6-suit-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-suit-room",
+      "name": "Suit room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.021196265787747137,
+        "pitch": -0.012933052961138003,
+        "fov": 0.9138126599488581
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5073012727767718,
+          "pitch": 0.007248050918764903,
+          "rotation": 0.7853981633974483,
+          "target": "7-tf-guest-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-tf-guest-room",
+      "name": "TF Guest room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9138126599488581
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Dr. Meeran Residence - toilet update",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
